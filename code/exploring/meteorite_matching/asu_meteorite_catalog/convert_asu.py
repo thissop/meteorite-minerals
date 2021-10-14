1aa94331962b6ba2c5e95404f5bd352789bfe652
thissop <68932815+thissop@users.noreply.github.com>
2021-10-14 09:37:57 -0700

    :+1:

--- code/exploring/meteorite_matching/asu_meteorite_catalog/convert_asu.py
+++ code/exploring/meteorite_matching/asu_meteorite_catalog/convert_asu.py
@@ -12,35 +12,53 @@ abs_file_path = os.path.join(script_dir, rel_path)
 names = np.array([])
 types = np.array([])
 
+m_types = []
+
 with open(abs_file_path, 'r') as f:
     for line in f:
+        print(line)
         if 'Meteorite	Name ASU' not in line: 
+            
+            line = re.sub(' +',' ', line)
+            line = line.replace('(','')
+            line = line.replace(')', '')
+
+
+
             counter = 0
             for character in line:
                 counter+=1
                 if character.isnumeric(): 
-                    counter-=1
+                    counter-=2
                     break
             
+
             stop_index = counter
             name = line[0:stop_index]
+ 
+            csv_line = re.sub(' +', ',', line)
 
-            if '(' in name:
-                name = name[0:name.index('(')]
+
+            type_index_start = 0
+            type_index_end = 0
             
-            name = re.sub(' +',' ', name)
-
             counter = 0
             for character in line: 
                 counter+=1
-                if counter > stop_index and (character.isnumeric()==False):
-                    print(line)
-                    print(line[counter])
-                    break
-                    #quit()
- 
+                if not(character.isnumeric()): 
+                    if line[counter-2].isnumeric():
+                        type_index_start = counter
+                        break 
+            
+
+            sub_line_list = line[type_index_start:].split(' ')
+            
+            meteorite_type = sub_line_list[0]
+
+            m_types.append(meteorite_type)
+
         #line_list = re.sub(' +',',',line).split(',')
         
 
-
+print(list(set(m_types)))
 
